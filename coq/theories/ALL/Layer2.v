393000a94bf933d3835d3821ce739a5d2ca2ba85
Enactics Dev <enactics@local>
2026-09-01 17:07:50 +0800

    S04: 恢复干净版本Layer2.v - 从commit 678468f恢复，编译通过，6个Admitted待补完
    恢复原因：之前的替换脚本把Layer2.v搞乱了（par_elim等引理被误删，证明结构混乱）
    干净版本状态：编译通过，subject_reduction完整证明，6个标准技术引理Admitted
    6个Admitted：par_elim/out_elim/in_elim/substitution_lemma/congruence_preserves_typing/progress
    下一步：叫DeepSeek补完这6个Admitted，然后推进L3

--- coq/theories/ALL/Layer2.v
+++ coq/theories/ALL/Layer2.v
@@ -3,7 +3,7 @@
    Layer 2: operational semantics + subject reduction
    Dependencies: Layer1.v (syntax, typing, renaming)
    ===================================================================== *)
-From Stdlib Require Import List PeanoNat Lia ClassicalEpsilon FunctionalExtensionality.
+From Stdlib Require Import List PeanoNat Lia ClassicalEpsilon.
 Require Import ALL.Layer1.
 Import ListNotations.
 
@@ -68,7 +68,6 @@ Inductive congruence : proc -> proc -> Prop :=
    --------------------------------------------------------------------- *)
 Inductive reduce : proc -> proc -> Prop :=
   | red_tau : forall P, reduce (PTau P) P
-  | red_out : forall P x y, reduce (POut x y P) (POut x y P)
   | red_comm : forall x y P Q,
       reduce (PPar (POut x y P) (PIn x Q)) (PPar P (comm_subst y Q))
   | red_par_l : forall P P' Q, reduce P P' -> reduce (PPar P Q) (PPar P' Q)
@@ -86,11 +85,9 @@ Inductive reduce_star : proc -> proc -> Prop :=
    --------------------------------------------------------------------- *)
 Inductive is_value : proc -> Prop :=
   | val_zero : is_value PZero
-  | val_var  : forall x, is_value (PVar x)
   | val_in   : forall x P, is_value (PIn x P)
   | val_res  : forall P, is_value P -> is_value (PRes P)
-  | val_rep  : forall P, is_value (PRep P)
-  | val_par  : forall P Q, is_value P -> is_value Q -> is_value (PPar P Q).
+  | val_rep  : forall P, is_value (PRep P).
 
 (* ---------------------------------------------------------------------
    6. Auxiliary lemmas
@@ -103,74 +100,27 @@ Proof.
     [right; split; [exact H1 | exact H2] | left; split; [exact H1 | exact H2]].
 Qed.
 
-Lemma split_zero_r : forall Gamma, split Gamma Gamma [].
-Proof.
-  intros Gamma. unfold split. intro n.
-  left. split. reflexivity.
-  simpl. left. reflexivity.
-Qed.
-
-Lemma split_zero_l : forall Gamma, split Gamma [] Gamma.
-Proof.
-  intros Gamma. unfold split. intro n.
-  right. split. reflexivity.
-  simpl. left. reflexivity.
-Qed.
-
-(* 构造split结合性所需的上下文合并函数：
-   c1是Some则取c2，否则取c3 *)
-Fixpoint merge_ctx (c1 c2 c3 : ctx) : ctx :=
-  match c1, c2, c3 with
-  | [], [], [] => []
-  | x1 :: xs1, x2 :: xs2, x3 :: xs3 =>
-      match x1 with
-      | Some _ => x2 :: merge_ctx xs1 xs2 xs3
-      | None => x3 :: merge_ctx xs1 xs2 xs3
-      end
-  | _, _, _ => []
-  end.
-
-Lemma get_merge_ctx_some : forall c1 c2 c3 n v,
-  get c1 n = Some v -> get (merge_ctx c1 c2 c3) n = get c2 n.
-Proof. Admitted.
-
-Lemma get_merge_ctx_none : forall c1 c2 c3 n,
-  (get c1 n = None \/ get c1 n = Some None) ->
-  get (merge_ctx c1 c2 c3) n = get c3 n.
-Proof. Admitted.
-
-Lemma split_assoc : forall Gamma Gamma1 Gamma2 Gamma11 Gamma12,
-  split Gamma Gamma1 Gamma2 -> split Gamma1 Gamma11 Gamma12 ->
-  exists Gamma2', split Gamma Gamma11 Gamma2' /\ split Gamma2' Gamma12 Gamma2.
-Proof. Admitted.
-
 Lemma use_set_none : forall Gamma x T Gamma',
   use Gamma x T Gamma' -> get Gamma' x = Some None.
-Proof. Admitted.
+Proof.
+  intros Gamma x T Gamma' [Hx HGamma']. subst Gamma'.
+  apply set_none_self. apply get_Some_lt in Hx. exact Hx.
+Qed.
 
 (* Key lemma: in linear type system, a channel cannot be used by both
    an output and an input in parallel composition. This makes red_comm
    vacuously true. *)
 Lemma par_elim : forall Gamma P Q, typed Gamma (PPar P Q) ->
   exists Gamma1 Gamma2, split Gamma Gamma1 Gamma2 /\ typed Gamma1 P /\ typed Gamma2 Q.
-Proof.
-  intros Gamma P Q H. inversion H; subst.
-  do 2 eexists. repeat split; eassumption.
-Qed.
+Proof. Admitted.
 
 Lemma out_elim : forall Gamma x y P, typed Gamma (POut x y P) ->
   exists Gamma1 Gamma2 T i o, use Gamma x (TChan i o T) Gamma1 /\ o = true /\ use Gamma1 y T Gamma2 /\ typed Gamma2 P.
-Proof.
-  intros Gamma x y P H. inversion H; subst.
-  do 5 eexists. split; [eassumption | split; [try reflexivity; try eassumption | split; [eassumption | eassumption]]].
-Qed.
+Proof. Admitted.
 
 Lemma in_elim : forall Gamma x P, typed Gamma (PIn x P) ->
   exists Gamma1 T i o, use Gamma x (TChan i o T) Gamma1 /\ i = true /\ typed (Some T :: Gamma1) P.
-Proof.
-  intros Gamma x P H. inversion H; subst.
-  do 4 eexists. split; [eassumption | split; [try reflexivity; try eassumption | eassumption]].
-Qed.
+Proof. Admitted.
 
 Lemma no_parallel_channel_sharing : forall Gamma x y P Q,
   ~ typed Gamma (PPar (POut x y P) (PIn x Q)).
@@ -182,288 +132,26 @@ Proof.
   unfold use in Huse1, Huse. destruct Huse1 as [Hx1 _], Huse as [Hx2 _].
   unfold split in Hs. specialize (Hs x).
   destruct Hs as [[Hg1 [Hg2 | Hg2]] | [Hg2 [Hg1 | Hg1]]].
-  - rewrite Hg2 in Hx2. inversion Hx2.
-  - rewrite Hg2 in Hx2. injection Hx2 as Hx2'. inversion Hx2'.
-  - rewrite Hg1 in Hx1. inversion Hx1.
-  - rewrite Hg1 in Hx1. injection Hx1 as Hx1'. inversion Hx1'.
+  - rewrite Hg2 in Hx2. discriminate.
+  - rewrite Hg2 in Hx2. injection Hx2 as Hx2'. discriminate.
+  - rewrite Hg1 in Hx1. discriminate.
+  - rewrite Hg1 in Hx1. injection Hx1 as Hx1'. discriminate.
 Qed.
 
 (* ---------------------------------------------------------------------
-   7. Substitution lemma
+   7. Substitution lemma (admitted for next iteration)
    --------------------------------------------------------------------- *)
-Lemma subst_name_eq : forall m k, subst_name m k k = m.
-Proof.
-  intros m k. unfold subst_name.
-  destruct (Nat.eqb_spec k k) as [H | H].
-  - reflexivity.
-  - contradiction H.
-Qed.
-
-Lemma subst_name_neq : forall m k n, n <> k -> n <= k -> subst_name m k n = n.
-Proof.
-  intros m k n Hneq Hle. unfold subst_name.
-  destruct (Nat.eqb_spec n k) as [Heq | Hneq'].
-  - contradiction Hneq.
-  - destruct (Nat.leb_spec n k) as [Hle' | Hgt].
-    + reflexivity.
-    + lia.
-Qed.
-
-Lemma subst_name_gt : forall m k n, n > k -> subst_name m k n = n - 1.
-Proof.
-  intros m k n Hgt. unfold subst_name.
-  destruct (Nat.eqb_spec n k) as [Heq | Hneq].
-  - lia.
-  - destruct (Nat.leb_spec n k) as [Hle | Hgt'].
-    + lia.
-    + reflexivity.
-Qed.
-
-Lemma subst_var_typed : forall Gamma T y Q,
-  typed (Some T :: Gamma) Q -> get Gamma y = Some (Some T) ->
-  typed Gamma (subst_var y 0 Q).
-Proof.
-  intros Gamma T y Q Ht Hget.
-  remember (Some T :: Gamma) as Gamma'.
-  remember 0 as k.
-  revert Gamma T y Q Ht Hget HeqGamma' Heqk.
-  induction Ht; intros Gamma0 T0 y0 Q0 Ht0 Hget0 HeqGamma' Heqk; subst.
-  - (* ty_zero *)
-    simpl. apply ty_zero.
-  - (* ty_var *)
-    simpl. destruct (Nat.eqb_spec x y0).
-    + (* x = y0 *)
-      subst. rewrite subst_name_eq.
-      apply ty_var. exact Hget0.
-    + (* x <> y0 *)
-      destruct (Nat.leb_spec x y0).
-      * (* x <= y0 *)
-        rewrite subst_name_neq; try assumption.
-        apply ty_var. 
-        simpl in H. 
-        destruct x.
-        -- simpl in H. inversion Hget.
-        -- simpl in H. exact H.
-      * (* x > y0 *)
-        rewrite subst_name_gt; try lia.
-        apply ty_var.
-        simpl in H.
-        destruct x.
-        -- simpl in H. lia.
-        -- simpl in H. 
-           destruct (Nat.eqb_spec x y0).
-           ++ subst. simpl in H. inversion Hget.
-           ++ simpl in H. exact H.
-  - (* ty_tau *)
-    simpl. apply ty_tau. 
-    apply IHHt with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-    exact Ht0.
-  - (* ty_out *)
-    simpl.
-    eapply ty_out.
-    + unfold use. split.
-      * simpl in Huse1. destruct Huse1 as [Hget1 Hset1].
-        unfold use in Huse1.
-        destruct Huse1 as [Hget1' Hset1'].
-        simpl in Hget1'.
-        destruct x.
-        -- simpl in Hget1'. inversion Hget.
-        -- simpl in Hget1'. 
-           destruct (Nat.eqb_spec x y0).
-           ++ subst. simpl in Hget1'. inversion Hget.
-           ++ simpl in Hget1'. exact Hget1'.
-      * simpl. 
-        destruct x.
-        -- simpl. reflexivity.
-        -- simpl.
-           destruct (Nat.eqb_spec x y0).
-           ++ subst. simpl. reflexivity.
-           ++ simpl. reflexivity.
-    + exact Ho.
-    + unfold use. split.
-      * simpl in Huse2. destruct Huse2 as [Hget2 Hset2].
-        unfold use in Huse2.
-        destruct Huse2 as [Hget2' Hset2'].
-        simpl in Hget2'.
-        destruct y.
-        -- simpl in Hget2'. inversion Hget.
-        -- simpl in Hget2'.
-           destruct (Nat.eqb_spec y y0).
-           ++ subst. simpl in Hget2'. inversion Hget.
-           ++ simpl in Hget2'. exact Hget2'.
-      * simpl.
-        destruct y.
-        -- simpl. reflexivity.
-        -- simpl.
-           destruct (Nat.eqb_spec y y0).
-           ++ subst. simpl. reflexivity.
-           ++ simpl. reflexivity.
-    + apply IHHt with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-      exact Ht0.
-  - (* ty_in *)
-    simpl.
-    eapply ty_in.
-    + unfold use. split.
-      * simpl in Huse. destruct Huse as [Hget1 Hset1].
-        unfold use in Huse.
-        destruct Huse as [Hget1' Hset1'].
-        simpl in Hget1'.
-        destruct x.
-        -- simpl in Hget1'. inversion Hget.
-        -- simpl in Hget1'.
-           destruct (Nat.eqb_spec x y0).
-           ++ subst. simpl in Hget1'. inversion Hget.
-           ++ simpl in Hget1'. exact Hget1'.
-      * simpl.
-        destruct x.
-        -- simpl. reflexivity.
-        -- simpl.
-           destruct (Nat.eqb_spec x y0).
-           ++ subst. simpl. reflexivity.
-           ++ simpl. reflexivity.
-    + exact Hi.
-    + apply IHHt with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-      simpl. 
-      destruct (Nat.eqb_spec y0 y0).
-      * reflexivity.
-      * contradiction n.
-  - (* ty_par *)
-    simpl.
-    eapply ty_par.
-    + exact Hs.
-    + apply IHHt1 with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-      exact Ht0.
-    + apply IHHt2 with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-      exact Ht0.
-  - (* ty_res *)
-    simpl.
-    apply ty_res.
-    apply IHHt with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-    simpl.
-    destruct (Nat.eqb_spec y0 y0).
-    * reflexivity.
-    * contradiction n.
-  - (* ty_rep *)
-    simpl. apply ty_rep.
-    apply IHHt with (Gamma := Gamma0) (T := T0) (y := y0); try reflexivity.
-    exact Ht0.
-Qed.
-
 Theorem substitution_lemma : forall Gamma T y Q,
   typed (Some T :: Gamma) Q -> get Gamma y = Some (Some T) ->
   typed Gamma (subst_var y 0 Q).
-Proof.
-  intros Gamma T y Q Ht Hget.
-  apply subst_var_typed with (Gamma := Gamma) (T := T) (y := y).
-  - exact Ht.
-  - exact Hget.
-Qed.
+Proof. Admitted.
 
 (* ---------------------------------------------------------------------
-   8. Congruence preserves typing
+   8. Congruence preserves typing (admitted for next iteration)
    --------------------------------------------------------------------- *)
-Lemma congruence_preserves_typing_both : forall P Q,
-  congruence P Q ->
-  (forall Gamma, typed Gamma P -> typed Gamma Q) /\ (forall Gamma, typed Gamma Q -> typed Gamma P).
-Proof.
-  intros P Q Hc.
-  induction Hc as [
-    P
-    | P Q Hc IH
-    | P Q R Hc1 IH1 Hc2 IH2
-    | P Q
-    | P Q R
-    | P
-    | P Q Hfv
-    | P
-    | P P' Q Q' Hc1 IH1 Hc2 IH2
-    | P P' Hc IH
-    | P P' Hc IH
-  ].
-  - (* cong_refl *)
-    split; intro Gamma; intro H; exact H.
-  - (* cong_sym *)
-    split.
-    + intro Gamma. intro Ht. destruct IH as [H1 H2]. exact (H2 Gamma Ht).
-    + intro Gamma. intro Ht. destruct IH as [H1 H2]. exact (H1 Gamma Ht).
-  - (* cong_trans *)
-    split.
-    + intro Gamma. intro Ht. destruct IH1 as [H11 H12]. destruct IH2 as [H21 H22].
-      exact (H21 Gamma (H11 Gamma Ht)).
-    + intro Gamma. intro Ht. destruct IH1 as [H11 H12]. destruct IH2 as [H21 H22].
-      exact (H12 Gamma (H22 Gamma Ht)).
-  - (* cong_par_comm *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. apply split_sym. eassumption. eassumption. eassumption.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. apply split_sym. eassumption. eassumption. eassumption.
-  - (* cong_par_assoc *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      apply par_elim in Ht. destruct Ht as [Gamma1 [Gamma2 [Hs [HP HQR]]]].
-      apply par_elim in HQR. destruct HQR as [Gamma21 [Gamma22 [Hs2 [HQ HR]]]].
-      destruct (split_assoc Gamma Gamma1 Gamma2 Gamma21 Gamma22 Hs Hs2) as [Gamma2' [Hs' Hs'']].
-      eapply ty_par.
-      * exact Hs'.
-      * eapply ty_par. exact Hs''. exact HP. exact HQ.
-      * exact HR.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      apply par_elim in Ht. destruct Ht as [Gamma1 [Gamma2 [Hs [HPQ HR]]]].
-      apply par_elim in HPQ. destruct HPQ as [Gamma11 [Gamma12 [Hs2 [HP HQ]]]].
-      eapply ty_par.
-      * eapply ty_par. exact Hs2. exact HP. exact HQ.
-      * exact HR.
-      * exact Hs.
-  - (* cong_par_zero *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      inversion H0; subst. exact H1.
-    + intro Gamma. intro Ht. eapply ty_par. eapply split_zero_r. exact Ht. apply ty_zero.
-  - (* cong_res_par *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. eapply split_zero_l.
-      eapply ty_res. destruct IH1 as [H11 H12]. exact (H11 _ H0).
-      exact H1.
-    + intro Gamma. intro Ht. inversion Ht; subst. inversion H0; subst.
-      eapply ty_res. destruct IH1 as [H11 H12]. exact (H12 _ H4).
-  - (* cong_rep_unfold *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. eapply split_zero_l. exact H0. eapply ty_rep. exact H0.
-    + intro Gamma. intro Ht. inversion Ht; subst. inversion H0; subst. exact H4.
-  - (* cong_par_cong *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. exact H0.
-      destruct IH1 as [H11 H12]. exact (H11 _ H1).
-      destruct IH2 as [H21 H22]. exact (H21 _ H2).
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_par. exact H0.
-      destruct IH1 as [H11 H12]. exact (H12 _ H1).
-      destruct IH2 as [H21 H22]. exact (H22 _ H2).
-  - (* cong_res_cong *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_res. destruct IH as [H1 H2]. exact (H1 _ H0).
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_res. destruct IH as [H1 H2]. exact (H2 _ H0).
-  - (* cong_tau_cong *)
-    split.
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_tau. destruct IH as [H1 H2]. exact (H1 _ H0).
-    + intro Gamma. intro Ht. inversion Ht; subst.
-      eapply ty_tau. destruct IH as [H1 H2]. exact (H2 _ H0).
-Qed.
-
 Theorem congruence_preserves_typing : forall P P' Gamma,
   congruence P P' -> typed Gamma P -> typed Gamma P'.
-Proof.
-  intros P P' Gamma Hc Ht.
-  destruct (congruence_preserves_typing_both P P' Hc) as [H1 H2].
-  exact (H1 Gamma Ht).
-Qed.
+Proof. Admitted.
 
 (* ---------------------------------------------------------------------
    9. Subject Reduction (FULLY PROVED)
@@ -476,7 +164,6 @@ Proof.
   intros Gamma P P' Ht Hr. revert Gamma Ht.
   induction Hr as [
     P
-    | P x y
     | x y P Q
     | P P' Q Hr IH
     | P Q Q' Hr IH
@@ -485,8 +172,6 @@ Proof.
   ]; intros Gamma Ht.
   - (* red_tau *)
     inversion Ht; subst; assumption.
-  - (* red_out: trivial, P' = P *)
-    assumption.
   - (* red_comm: impossible by no_parallel_channel_sharing *)
     exfalso. eapply no_parallel_channel_sharing. exact Ht.
   - (* red_par_l *)
@@ -509,53 +194,11 @@ Proof.
 Qed.
 
 (* ---------------------------------------------------------------------
-   10. Progress
+   10. Progress (admitted for next iteration)
    --------------------------------------------------------------------- *)
-(* 广义progress：对任意上下文，类型良好的进程要么是值，要么可以归约 *)
-Theorem progress_general : forall Gamma P, typed Gamma P ->
-  is_value P \/ exists P', reduce P P'.
-Proof.
-  intros Gamma P Ht.
-  induction Ht as [
-    Gamma
-    | Gamma x T Hget
-    | Gamma P H IH
-    | Gamma x y P i o T Gamma1 Gamma2 Huse1 Ho Huse2 Hbody IH
-    | Gamma x P i o T Gamma1 Huse Hi Hbody IH
-    | Gamma P Q Gamma1 Gamma2 Hs HP IHP HQ IHQ
-    | Gamma P T H IH
-    | Gamma P H IH
-  ].
-  - (* ty_zero *)
-    left. apply val_zero.
-  - (* ty_var *)
-    left. apply val_var.
-  - (* ty_tau *)
-    right. exists P. apply red_tau.
-  - (* ty_out *)
-    right. exists (POut x y P). apply red_out.
-  - (* ty_in *)
-    left. apply val_in.
-  - (* ty_par *)
-    destruct IHP as [HvalP | [P' HredP]].
-    + destruct IHQ as [HvalQ | [Q' HredQ]].
-      * left. apply val_par. exact HvalP. exact HvalQ.
-      * right. exists (PPar P Q'). apply red_par_r. exact HredQ.
-    + right. exists (PPar P' Q). apply red_par_l. exact HredP.
-  - (* ty_res *)
-    destruct IH as [HvalP | [P' HredP]].
-    + left. apply val_res. exact HvalP.
-    + right. exists (PRes P'). apply red_res. exact HredP.
-  - (* ty_rep *)
-    left. apply val_rep.
-Qed.
-
-(* 闭进程的progress是广义版本的特例 *)
 Theorem progress : forall P, typed [] P ->
   is_value P \/ exists P', reduce P P'.
-Proof.
-  intros P Ht. apply progress_general with (Gamma := []). exact Ht.
-Qed.
+Proof. Admitted.
 
 (* =====================================================================
    LAYER 2 SUMMARY
@@ -565,7 +208,7 @@ Qed.
      Core insight: red_comm is vacuously true because linear split
      prevents channel sharing between parallel components.
    - substitution_lemma, congruence_preserves_typing, progress:
-     FULLY PROVED
+     admitted for next iteration (standard technical lemmas)
    ===================================================================== *)
 
 (* === END === *)
